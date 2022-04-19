dc3f7e8c922e33cb74072ca9171596a47721656e
alvaradoarturo <aalva058@ucr.edu>
2022-04-18 23:23:10 -0700

    Implemented function to handle numbers

--- calculator/scripts/calculations.js
+++ calculator/scripts/calculations.js
@@ -1,10 +1,7 @@
 let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
-
-document.querySelector('.calc-buttons').addEventListener('click', function(event) {
-    buttonClick(event.target.value);
-})
+const screen = document.querySelector('.value');
 
 function buttonClick(value) {
     if(isNaN(parseInt(value))) {
@@ -12,4 +9,29 @@ function buttonClick(value) {
     } else {
         handleNumber(value);
     }
+    rerender();
 }
+
+function handleNumber(value) {
+    if(buffer === "0") {
+        buffer = value; 
+    } else {
+        buffer += value;
+    }
+}
+
+function handleSymbol(value) {
+    // TODO
+}
+
+function rerender() {
+    screen.innerText = buffer;
+}
+
+function init() {
+    document.querySelector(".calc-buttons").addEventListener("click", function(event) {
+      buttonClick(event.target.innerText);
+    });
+}
+  
+init();
