49139cda6c0daff515b109ecaa66bb16be54d93d
alvaradoarturo <aalva058@ucr.edu>
2022-04-20 20:46:01 -0700

    Implemented backspace

--- calculator/scripts/calculations.js
+++ calculator/scripts/calculations.js
@@ -1,6 +1,7 @@
 let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
+console.log(previousOperator);
 const screen = document.querySelector('.value');
 
 function buttonClick(value) {
@@ -19,9 +20,39 @@ function handleNumber(value) {
         buffer += value;
     }
 }
+console.log(parseInt('+'));
 
 function handleSymbol(value) {
-    // TODO
+    switch(value) {
+        case "C":
+            buffer = '0';
+            break;
+        case "<-":
+            console.log("Enters backspace");
+            if(buffer.length === 1 ){
+                buffer = '0';
+                console.log(buffer);
+            }
+            else {
+                buffer = buffer.slice(0, -1);
+            }
+            break;
+        case "+":
+            if(previousOperator === undefined){
+                runningTotal = buffer;
+                buffer = 0;
+                previousOperator = '+';
+            }
+            else {
+                console.log(parseInt(previousOperator)); 
+            }
+            break;
+        default:
+          // code block
+        console.log("Will rerender"); 
+      }
+      console.log("Rerenders");
+      rerender();
 }
 
 function rerender() {
